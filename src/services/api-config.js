cf1a56af9b34c1a1218c3f2aded7b2979b859ce3
Carol Gonzaga <dev.carolgonzaga@gmail.com>
2025-10-16 10:58:31 -0300

    feat: atualiza a URL da API para o Render

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,4 +1,4 @@
 // Define o endereço base da API
 export const apiConfig = {
-    baseURL: "https://petshop-api-production.up.railway.app",
+    baseURL: "https://petshop-api-pmxi.onrender.com",
 };
